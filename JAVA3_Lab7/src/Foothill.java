5222d972125c94d50129011f3cf954920605e5bd
Sheila Xu <Sheila Xu@s>
2016-02-16 10:28:08 -0800

    Add client

--- JAVA3_Lab7/src/Foothill.java
+++ JAVA3_Lab7/src/Foothill.java
@@ -1,4 +1,27 @@
 
 public class Foothill {
+	
+	 public static void main(String[] args) throws Exception {
+			final int ARRAY_SIZE = 10000;
+		    
+		    Integer[] arrayOfInts1 = new Integer[ARRAY_SIZE];
+		    Integer[] arrayOfInts2 = new Integer[ARRAY_SIZE];
+		    
+		    int[] gapArray = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024,
+		       2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288,
+		       1048576};
+		    int[] sedgewickArray = new int[30]; // to be computed using formulas
+		    // ... other gap arrays ...
+		 
+		    // fill distinct arrays with identical random values so we can compare gaps
+
+		    
+
+		    shellSortX(arrayOfInts1, gapArray);  // time this
+		    
+
+		    
+		    shellSortX(arrayOfInts2, sedgewickArray);  // time this
+	 }
 
 }
